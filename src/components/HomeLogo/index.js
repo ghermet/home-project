0cc9b81e06c655ae59e93bff8472b9e37bd34f92
Guillaume Hermet <guillaume.hermet13@gmail.com>
2018-10-27 16:19:01 +0200

    added HomeLogo component

---
+++ src/components/HomeLogo/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <img
+    alt="Home"
+    src="https://www.home.ht/wp-content/uploads/2017/01/122.png"
+    srcSet="https://www.home.ht/wp-content/uploads/2017/01/122.png 1x, https://www.home.ht/wp-content/uploads/2017/01/122.png 2x"
+    style={{ height: 75 }}
+  />
+);
