b0069a5e9d38268497f54a145b894ab88c347128
Guillaume Hermet <guillaume.hermet13@gmail.com>
2018-10-27 16:11:39 +0200

    added Row component

---
+++ src/components/Row/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+import './Row.scss';
+
+const Row = ({ className, style, children, ...rest }) => (
+  <div
+    {...rest}
+    className={cx(['row', className])}
+    style={{ ...style, display: 'flex', flexWrap: 'wrap', flexDirection: 'row' }}
+  >
+    {children}
+  </div>
+);
+
+Row.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+  className: PropTypes.string,
+  style: PropTypes.string,
+};
+
+Row.defaultProps = {
+  className: '',
+  style: {},
+};
+
+export default Row;
